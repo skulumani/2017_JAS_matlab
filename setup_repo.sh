070e796016f9d57ba9a44a3ac985440d8bd9c283
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-01-17 16:21:02 -0500

    modify repo script

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # add the correct remote repositories
-git_repo="git@github.com:skulumani/2015_ACTA_matlab.git"
-bb_repo="git@bitbucket.org:shankarkulumani/2015_acta_matlab.git"
-fdcl_repo="git@github.com:fdcl-gwu/2015_ACTA_matlab.git"
+git_repo="git@github.com:skulumani/2017_JAS_matlab.git"
+bb_repo="git@bitbucket.org:shankarkulumani/2017_jas_matlab.git"
 
 printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
 printf "Github: $git_repo\n"
-printf "Bitbucket: $bb_repo\n"
-printf "FDCL: $fdcl_repo\n\n"
+printf "Bitbucket: $bb_repo\n\n"
+
 
 git remote set-url origin --push --add $bb_repo
-git remote set-url origin --push --add $fdcl_repo
 git remote set-url origin --push --add $git_repo
 
 printf "Finished! The new remotes are listed.\n"
