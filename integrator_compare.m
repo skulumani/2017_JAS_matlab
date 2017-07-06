8776387ea973d5b2eec60656f1d29d939c9156a5
Shankar Kulumani <shanks.k@gmail.com>
2017-07-06 10:05:11 -0400

    Compute and plot ode4 results

--- integrator_compare.m
+++ integrator_compare.m
@@ -13,13 +13,15 @@ height = 2;
 
 % arrays to save the data
 Ehist_ode45 = zeros(length(num_steps), 1e7);
+Ehist_ode4 = zeros(length(num_steps), 1e7);
 Ehist_trap = zeros(length(num_steps), 1e7);
 meanE_ode45 = zeros(length(num_steps),1);
+meanE_ode = zeros(length(num_steps), 1);
 meanE_trap = zeros(length(num_steps),1);
 cputime_ode45 = zeros(length(num_steps), 1);
+cputime_ode = zeros(length(num_steps), 1);
 cputime_trap = zeros(length(num_steps), 1);
 step_size = zeros(length(num_steps), 1);
-
 
 for ii = 1:length(num_steps)
     ns = num_steps(ii);
@@ -32,10 +34,10 @@ for ii = 1:length(num_steps)
     ode45_end = toc(ode45_st);
     
     % propogate using ODE4
-    % ode4_st = tic;
-    % t_ode4 = t_vec;
-    % state_ode4 =ode4(@(t,state)pcrtbp_ode(t,state,constants.mu),t_vec, x0);
-    % ode4_end = toc(ode4_st);
+    ode4_st = tic;
+    t_ode4 = t_vec;
+    state_ode4 =ode4(@(t,state)pcrtbp_ode(t,state,constants.mu),t_vec, x0);
+    ode4_end = toc(ode4_st);
     
     % propogate using variational integrator rectangle
     % vir_st = tic;
@@ -50,7 +52,7 @@ for ii = 1:length(num_steps)
     % compute the energy difference
     % calculate jacobi energies for both
     [E_ode45] = energyconst(state_ode45,constants.mu);
-    % [E_ode4] = energyconst(state_ode4,constants.mu);
+    [E_ode4] = energyconst(state_ode4,constants.mu);
     % [E_rect] = energyconst(state_rect,constants.mu);
     [E_trap] = energyconst(state_trap,constants.mu);
     
@@ -60,28 +62,32 @@ for ii = 1:length(num_steps)
     fprintf('h = %5.2e nondim = %5.2f sec = %5.2f days\n\n',h,h*constants.t_scale, h*constants.t_scale/86400);
     
     fprintf('ODE45 energy drift %12.10e\n',mean(abs(E_ode45(end)-E_ode45(1))));
-    % fprintf('ODE4 energy drift %12.10e\n',mean(abs(E_ode4(end)-E_ode4(1))));
+    fprintf('ODE4 energy drift %12.10e\n',mean(abs(E_ode4(end)-E_ode4(1))));
     % fprintf('VI RECT energy drift %12.10e\n',mean(abs(E_rect-E_rect(1))));
     fprintf('VI TRAP Energy drift %12.10e\n',mean(abs(E_trap-E_trap(1))));
     
     fprintf('ODE45 mean E var %12.10e\n', var(E_ode45));
-    % fprintf('ODE4 mean E var %12.10e\n', var(E_ode4));
+    fprintf('ODE4 mean E var %12.10e\n', var(E_ode4));
     % fprintf('VI RECT mean E var %12.10e\n',var(E_rect));
     fprintf('VI TRAP mean E var %12.10e\n',var(E_trap));
     
     fprintf('ODE45 run time %5.2f sec\n',ode45_end);
-    % fprintf('ODE4 run time %5.2f sec\n',ode4_end);
+    fprintf('ODE4 run time %5.2f sec\n',ode4_end);
     % fprintf('VI RECT run time %5.2f sec\n',vir_end);
     fprintf('VI TRAP run time %5.2f sec\n',vit_end);
     
     % store in arrays
     Ehist_ode45(ii,1:length(E_ode45)) = E_ode45;
+    Ehist_ode4(ii, 1:length(E_ode4))
     Ehist_trap(ii,1:length(E_trap)) = E_trap;
+
     meanE_ode45(ii) = mean(abs(E_ode45 - E_ode45(1)));
+    meanE_ode4(ii) = mean(abs(E_ode4 - E_ode4(1)));
     meanE_trap(ii) = mean(abs(E_trap - E_trap(1)));
 
     cputime_ode45(ii) = ode45_end;
+    cputime_ode4(ii) = ode4_end;
     cputime_trap(ii) = vit_end;
 end
 
-save('E_comparison.mat', 'Ehist_ode45', 'Ehist_trap', 'meanE_ode45', 'meanE_trap', 'cputime_ode45', 'cputime_trap', 'num_steps', 'step_size', '-v7.3')
+save('E_comparison.mat', 'Ehist_ode45', 'Ehist_ode4', 'Ehist_trap', 'meanE_ode45', 'meanE_ode4', 'meanE_trap', 'cputime_ode45', 'cputime_ode4', 'cputime_trap', 'num_steps', 'step_size', '-v7.3')
---
+++ plot_integrator_compare.m
@@ -0,0 +1,62 @@
+function plot_integrator_compare(filename)
+
+    load(filename);
+    t0 = 0;
+    tf = 200; % figure out how to dimensionalize time
+    set(0,'DefaultAxesFontSize',22);
+
+    e_fig = figure('PaperPositionMode','auto'); % energy history
+    h_fig = figure('PaperPositionMode', 'auto'); % mean \Delta E
+    t_fig = figure('PaperPositionMode', 'auto'); % cpu time
+
+    % plot trajectories and energy differences
+    set(0,'CurrentFigure',e_fig)
+    grid on
+    hold all
+    xlabel('$t$ (nondim) ','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+    ylabel('$ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+
+    set(0, 'CurrentFigure', h_fig)
+    grid on 
+    hold all
+    xlabel('$h$ (nondim)','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+    ylabel('Mean $ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+
+    set(0, 'CurrentFigure', t_fig)
+    grid on
+    hold all
+    xlabel('$h$ (nondim)', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
+    ylabel('CPU Time (sec)', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
+
+    % add to plot
+    % loop over the changes in the number of steps/step size
+    for ii = 1:length(num_steps)
+        ns = num_steps(ii);
+        t_vec = linspace(t0, tf, ns);
+        h = (tf - t0) / ns;
+        set(0,'CurrentFigure',e_fig)
+        plot(t_vec, abs(Ehist_ode45(ii,1:ns)-Ehist_ode45(ii,1)), 'r')
+        plot(t_vec, abs(Ehist_ode4(ii,1:ns) - Ehist_ode4(ii,1)), 'b')
+        plot(t_vec, abs(Ehist_trap(ii,1:ns)-Ehist_trap(ii,1)), 'g')
+
+        set(0, 'CurrentFigure', h_fig)
+        loglog(h, mean(abs(Ehist_ode45(ii,1:ns) - Ehist_ode45(ii,1))), 'ro')
+        loglog(h, mean(abs(Ehist_ode4(ii,1:ns) - Ehist_ode4(ii,1))), 'bs')
+        loglog(h, mean(abs(Ehist_trap(ii,1:ns) - Ehist_trap(ii,1))), 'gx')
+
+        set(0, 'CurrentFigure', t_fig)
+        loglog(h, cputime_ode45(ii), 'ro')
+        loglog(h, cputime_ode4(ii), 'bs')
+        loglog(h, cputime_trap(ii), 'gx')
+
+    end
+    % h=legend('RK45','VI TRAP');
+    % set(h,'interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times');
+
+    % print(e_fig,'-dpsc2', 'energy.eps')
+    % print(traj_fig,'-dpsc2', 'trajectory.eps')
+    % print(comp_fig,'-dpsc2', 'components.eps')
+
+    % print(e_fig,sprintf('./variational_integrator/energy_diff_h1%s%d.eps','e',step_exp))
+    % print(traj_fig,sprintf('./variational_integrator/trajectory_h1%s%d.eps','e',step_exp))
+    % print(comp_fig,sprintf('./variational_integrator/state_comp_h1%s%d.eps','e',step_exp))
