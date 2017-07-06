fa1f8d64f339f28aa86f910a29cc3af5830e8ef9
Shankar Kulumani <shanks.k@gmail.com>
2017-07-06 09:12:13 -0400

    Remove plotting functions to their own file"

--- integrator_compare.m
+++ integrator_compare.m
@@ -1,16 +1,8 @@
 % 25 Feb 2015 - script to compare performance of variational integrator and
 % ODE45 and normal euler integration
-clear all
-close all
-clc
+function [] = integrator_compare()
 
 addpath(genpath('./ode_solvers'));
-% define constants
-constants = crtbp_constants;
-set(0,'DefaultAxesFontSize',22);
-% define initial conditions
-% [x0, ~, E_i, ~] = periodic_orbit_pcrtbp(constants.l_point, constants.e_desired, constants);
-% x0 = x0';
 x0 = [0.75;0;0;0.2883]';
 t0 = 0;
 tf = 200; % figure out how to dimensionalize time
@@ -18,37 +10,15 @@ num_steps = 1 * 10 .^ [4, 5, 6, 7];
 width = 5;
 height = 2;
 
-% e_fig = figure('PaperPositionMode','auto');
-% h_fig = figure('PaperPositionMode', 'auto');
-% t_fig = figure('PaperPositionMode', 'auto');
-
 % arrays to save the data
-Ehist_ode45 = zeros(length(num_steps), 1e9);
-Ehist_trap = zeros(length(num_steps), 1e9);
+Ehist_ode45 = zeros(length(num_steps), 1e7);
+Ehist_trap = zeros(length(num_steps), 1e7);
 meanE_ode45 = zeros(length(num_steps),1);
 meanE_trap = zeros(length(num_steps),1);
 cputime_ode45 = zeros(length(num_steps), 1);
 cputime_trap = zeros(length(num_steps), 1);
 step_size = zeros(length(num_steps), 1);
 
-% plot trajectories and energy differences
-% set(0,'CurrentFigure',e_fig)
-% grid on
-% hold all
-% xlabel('$t$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-% ylabel('$ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-
-% set(0, 'CurrentFigure', h_fig)
-% grid on 
-% hold all
-% xlabel('Step size','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-% ylabel('Mean $ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-
-% set(0, 'CurrentFigure', t_fig)
-% grid on
-% hold all
-% xlabel('Step size', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
-% ylabel('CPU Time', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
 
 for ii = 1:length(num_steps)
     ns = num_steps(ii);
@@ -102,18 +72,6 @@ for ii = 1:length(num_steps)
     % fprintf('ODE4 run time %5.2f sec\n',ode4_end);
     % fprintf('VI RECT run time %5.2f sec\n',vir_end);
     fprintf('VI TRAP run time %5.2f sec\n',vit_end);
-    % add to plot
-    % set(0,'CurrentFigure',e_fig)
-    % plot(t_ode45, abs(E_ode45-E_ode45(1)), 'r')
-    % plot(t_trap, abs(E_trap-E_trap(1)), 'g')
-
-    % set(0, 'CurrentFigure', h_fig)
-    % loglog(h, mean(abs(E_ode45 - E_ode45(1))), 'ro')
-    % loglog(h, mean(abs(E_trap - E_trap(1))), 'gx')
-    
-    % set(0, 'CurrentFigure', t_fig)
-    % loglog(h, ode45_end, 'ro')
-    % loglog(h, vit_end, 'gx')
     
     % store in arrays
     Ehist_ode45(ii,1:length(E_ode45)) = E_ode45;
@@ -126,14 +84,3 @@ for ii = 1:length(num_steps)
 end
 
 save('E_comparison.mat', 'Ehist_ode45', 'Ehist_trap', 'meanE_ode45', 'meanE_trap', 'cputime_ode45', 'cputime_trap', 'num_steps', 'step_size', '-v7.3')
-% 
-% h=legend('RK45','VI TRAP');
-% set(h,'interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times');
-
-% print(e_fig,'-dpsc2', 'energy.eps')
-% print(traj_fig,'-dpsc2', 'trajectory.eps')
-% print(comp_fig,'-dpsc2', 'components.eps')
-
-% print(e_fig,sprintf('./variational_integrator/energy_diff_h1%s%d.eps','e',step_exp))
-% print(traj_fig,sprintf('./variational_integrator/trajectory_h1%s%d.eps','e',step_exp))
-% print(comp_fig,sprintf('./variational_integrator/state_comp_h1%s%d.eps','e',step_exp))
