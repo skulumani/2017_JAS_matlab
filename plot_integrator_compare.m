3f6e54e793afa325347a30f94212fa0969926d62
Shankar Kulumani <shanks.k@gmail.com>
2017-07-06 16:25:19 -0400

    Move plot definitions to end of function

--- plot_integrator_compare.m
+++ plot_integrator_compare.m
@@ -10,23 +10,6 @@ function plot_integrator_compare(filename)
     t_fig = figure('PaperPositionMode', 'auto'); % cpu time
 
     % plot trajectories and energy differences
-    set(0,'CurrentFigure',e_fig)
-    grid on
-    hold all
-    xlabel('$t$ (nondim) ','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-    ylabel('$ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-
-    set(0, 'CurrentFigure', h_fig)
-    grid on 
-    hold all
-    xlabel('$h$ (nondim)','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-    ylabel('Mean $ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
-
-    set(0, 'CurrentFigure', t_fig)
-    grid on
-    hold all
-    xlabel('$h$ (nondim)', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
-    ylabel('CPU Time (sec)', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
 
     % add to plot
     % loop over the changes in the number of steps/step size
@@ -35,21 +18,38 @@ function plot_integrator_compare(filename)
         t_vec = linspace(t0, tf, ns);
         h = (tf - t0) / ns;
         set(0,'CurrentFigure',e_fig)
-        plot(t_vec, abs(Ehist_ode45(ii,1:ns)-Ehist_ode45(ii,1)), 'r')
-        plot(t_vec, abs(Ehist_ode4(ii,1:ns) - Ehist_ode4(ii,1)), 'b')
-        plot(t_vec, abs(Ehist_trap(ii,1:ns)-Ehist_trap(ii,1)), 'g')
+        semilogy(t_vec, abs(Ehist_ode45(ii,1:ns)-Ehist_ode45(ii,1)), 'r')
+        hold all
+        semilogy(t_vec, abs(Ehist_ode4(ii,1:ns) - Ehist_ode4(ii,1)), 'b')
+        semilogy(t_vec, abs(Ehist_trap(ii,1:ns)-Ehist_trap(ii,1)), 'g')
 
         set(0, 'CurrentFigure', h_fig)
         loglog(h, mean(abs(Ehist_ode45(ii,1:ns) - Ehist_ode45(ii,1))), 'ro')
+        hold all
         loglog(h, mean(abs(Ehist_ode4(ii,1:ns) - Ehist_ode4(ii,1))), 'bs')
         loglog(h, mean(abs(Ehist_trap(ii,1:ns) - Ehist_trap(ii,1))), 'gx')
 
         set(0, 'CurrentFigure', t_fig)
         loglog(h, cputime_ode45(ii), 'ro')
+        hold all
         loglog(h, cputime_ode4(ii), 'bs')
         loglog(h, cputime_trap(ii), 'gx')
 
     end
+    set(0,'CurrentFigure',e_fig)
+    grid on
+    xlabel('$t$ (nondim) ','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+    ylabel('$ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+
+    set(0, 'CurrentFigure', h_fig)
+    grid on 
+    xlabel('$h$ (nondim)','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+    ylabel('Mean $ \Delta E$','interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times')
+
+    set(0, 'CurrentFigure', t_fig)
+    grid on
+    xlabel('$h$ (nondim)', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
+    ylabel('CPU Time (sec)', 'interpreter', 'latex', 'FontUnits', 'points', 'FontSize', 22, 'FontName', 'Times')
     % h=legend('RK45','VI TRAP');
     % set(h,'interpreter','latex','FontUnits','points','FontSize',22,'FontName','Times');
 
